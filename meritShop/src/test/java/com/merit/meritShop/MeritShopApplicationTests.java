1c9b31ba73211a0d840f6cffa04e4b8eb981b82b
jimin <cini1111@naver.com>
2022-05-30 20:11:02 +0900

    chore : 패키지 구조 설정 완료

---
+++ meritShop/src/test/java/com/merit/meritShop/MeritShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merit.meritShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeritShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
