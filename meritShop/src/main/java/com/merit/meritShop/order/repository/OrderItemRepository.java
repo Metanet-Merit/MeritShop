385e7eb46a53dc5b6944e910f953c5cd78fd14bf
jojooah <my_sg1@naver.com>
2022-06-10 09:34:12 +0900

    add:리뷰 페이지 작성

--- meritShop/src/main/java/com/merit/meritShop/order/repository/OrderItemRepository.java
+++ meritShop/src/main/java/com/merit/meritShop/order/repository/OrderItemRepository.java
@@ -1,4 +1,4 @@
-package com.merit.meritShop.order.repository;
+package com.merit.meritShop.order.Repository;
 
 import com.merit.meritShop.order.domain.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
