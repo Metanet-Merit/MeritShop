f3873d911ab19e3d4ecf5f5c04450c160c696ca2
sadertke <scalpi177@gmail.com>
2022-06-11 15:58:08 +0900

    delete: order/Repository

--- meritShop/src/main/java/com/merit/meritShop/order/repository/OrderItemRepository.java
+++ meritShop/src/main/java/com/merit/meritShop/order/repository/OrderItemRepository.java
@@ -2,8 +2,7 @@ package com.merit.meritShop.order.repository;
 
 import com.merit.meritShop.order.domain.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+햣
 }
