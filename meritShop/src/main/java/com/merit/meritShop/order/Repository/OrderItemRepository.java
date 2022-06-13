8737484c61f6d1023e48c1e554c39b51aeae1182
begrey <cini1111@naver.com>
2022-06-13 09:36:25 +0900

    fix: OrderItemRepository.java package name

--- meritShop/src/main/java/com/merit/meritShop/order/Repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.merit.meritShop.order.Repository;
-
-import com.merit.meritShop.order.domain.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
-}
--- meritShop/src/main/java/com/merit/meritShop/order/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.merit.meritShop.order.repository;
-
-import com.merit.meritShop.order.domain.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
-}
