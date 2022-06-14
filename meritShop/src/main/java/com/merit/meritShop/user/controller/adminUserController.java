07d3730758718160009ea4bd1e84669005da8504
begrey <cini1111@naver.com>
2022-06-14 17:15:41 +0900

    add: admin user 검색, 페이지네이션

--- meritShop/src/main/java/com/merit/meritShop/user/controller/adminUserController.java
+++ meritShop/src/main/java/com/merit/meritShop/user/controller/adminUserController.java
@@ -3,7 +3,10 @@ package com.merit.meritShop.user.controller;
 import com.merit.meritShop.user.dto.UserViewDto;
 import com.merit.meritShop.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping(value = "/admin")
 @Controller
@@ -23,10 +27,21 @@ public class adminUserController {
 
     @GetMapping("/user/list")
     public String userList(Model model, @PageableDefault Pageable pageable){
-        List<UserViewDto> userList = userService.getAllUserInfo();
+        Page<UserViewDto> userList = userService.getAllUserInfo(pageable);
         model.addAttribute("userList", userList);
+        log.debug("총 element 수 : {}, 전체 page 수 : {}, 페이지에 표시할 element 수 : {}, 현재 페이지 index : {}, 현재 페이지의 element 수 : {}",
+                userList.getTotalElements(), userList.getTotalPages(), userList.getSize(),
+                userList.getNumber(), userList.getNumberOfElements());
+
         return "admin/user/userList";
     }
+
+    @GetMapping("/user/search")    public String search(String keyword, Model model, @PageableDefault(direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<UserViewDto> searchList = userService.search(keyword, pageable);
+        model.addAttribute("userList", searchList);
+        return "admin/user/userList";
+    }
+
 
     @GetMapping("/user/detail")
     public String userDetail(Model model, @RequestParam Long userId){
--- meritShop/src/main/java/com/merit/meritShop/user/dto/UserViewDto.java
+++ meritShop/src/main/java/com/merit/meritShop/user/dto/UserViewDto.java
@@ -1,6 +1,12 @@
 package com.merit.meritShop.user.dto;
 
+import com.merit.meritShop.user.domain.User;
 import lombok.*;
+import org.springframework.data.domain.Page;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -32,4 +38,29 @@ public class UserViewDto {
     private String sex;
 
     private String zipcode;
+
+    public Page<UserViewDto> toDtoList(Page<User> user){
+        Page<UserViewDto> userViewDtoPage = user.map(m -> UserViewDto.builder()
+                .userId(m.getUserId())
+                .email(m.getEmail())
+                .userName(m.getUserName())
+                .loginType(m.getLoginType())
+                .role(m.getRole().equals("ROLE_USER") ? "일반 회원" : m.getRole().equals("ROLE_PREMIUM") ? "프리미엄 회원" : "블랙 회원")
+                .expireDate(getRemainDay(m.getRemainDay()))
+                .point(m.getPoint())
+                .build());
+        return userViewDtoPage;
+    }
+    public String getRemainDay(String remain) {
+        if (remain == null)
+            return "해당없음";
+        else {
+            Date now = new Date();
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(now);
+            cal.add(Calendar.DATE, Integer.parseInt(remain));
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            return sdf.format(cal.getTime());
+        }
+    }
 }
