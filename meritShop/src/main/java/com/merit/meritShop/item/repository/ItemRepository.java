6d1ae2b3df3a77fd51a5c8124f2072787dbcc518
jojooah <my_sg1@naver.com>
2022-06-15 16:53:29 +0900

    add:리뷰 카테고리별 상품 찾기

--- meritShop/src/main/java/com/merit/meritShop/item/repository/ItemRepository.java
+++ meritShop/src/main/java/com/merit/meritShop/item/repository/ItemRepository.java
@@ -4,9 +4,15 @@ import com.merit.meritShop.item.domain.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItemRepository extends JpaRepository<Item,Long> {
+public interface ItemRepository extends JpaRepository<Item, Long> {
     Page<Item> findAll(Pageable pageable);
+
+    //@Query("select i from Item i where i.category=:category")
+    Page<Item> findAllByCategory(String category, Pageable pageable);
+
+
 }
