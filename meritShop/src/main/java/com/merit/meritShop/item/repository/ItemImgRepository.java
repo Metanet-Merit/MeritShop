7a9f29549e708a0e511f2dde4429349776a0cc82
sadertke <scalpi177@gmail.com>
2022-06-17 12:00:16 +0900

    fix: 상품 이미지 변경 완료

--- meritShop/src/main/java/com/merit/meritShop/item/repository/ItemImgRepository.java
+++ meritShop/src/main/java/com/merit/meritShop/item/repository/ItemImgRepository.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 public interface ItemImgRepository extends JpaRepository<ItemImg,Long> {
     List<ItemImg> findAllByItem(Item item);
+    List<ItemImg> findAllByItemAndItemImgIdIn(Item item,List<Long>id);
     void deleteAllByItemAndItemImgIdNotIn(Item item,List<Long>id);
+    void deleteAllByItemItemId(Long itemId);
 
 }
