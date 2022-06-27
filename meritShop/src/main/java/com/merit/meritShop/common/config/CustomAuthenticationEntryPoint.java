f0a00e3dfa0f617a848483cd9786264e5525bc3d
begrey <cini1111@naver.com>
2022-06-27 21:37:37 +0900

    fix: spring security config 설정, sweet Alert

--- meritShop/src/main/java/com/merit/meritShop/common/config/CustomAuthenticationEntryPoint.java
+++ meritShop/src/main/java/com/merit/meritShop/common/config/CustomAuthenticationEntryPoint.java
@@ -1,6 +1,7 @@
 package com.merit.meritShop.common.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -20,8 +21,11 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException ex) throws IOException,
             ServletException {
         response.setContentType("text/html; charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        out.println("<script>alert('로그인이 필요한 서비스입니다. 로그인 해주세요'); " +
-                "location.href='http://localhost:8080/user/login';</script>"); out.flush();
+        request.setAttribute("msg", "로그인이 필요한 서비스입니다. 로그인 해주세요");
+        request.setAttribute("nextPage", "/c");
+        request.getRequestDispatcher("/err/denied-page").forward(request, response);
+//        PrintWriter out = response.getWriter();
+//        out.println("<script>alert('로그인이 필요한 서비스입니다. 로그인 해주세요'); " +
+//                "location.href='http://localhost:8080/user/login';</script>"); out.flush();
     }
 }
