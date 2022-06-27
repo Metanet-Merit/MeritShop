734f7076465a238433bc4d1728d09c9437fb5bd8
begrey <cini1111@naver.com>
2022-06-27 23:12:13 +0900

    add: 쿠폰 페이징 처리, 검색 기능 구현

--- meritShop/src/main/java/com/merit/meritShop/coupon/repository/CouponRepository.java
+++ meritShop/src/main/java/com/merit/meritShop/coupon/repository/CouponRepository.java
@@ -1,9 +1,13 @@
 package com.merit.meritShop.coupon.repository;
 
 import com.merit.meritShop.coupon.domain.Coupon;
+import com.merit.meritShop.user.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CouponRepository extends JpaRepository<Coupon,Long> {
+    Page<Coupon> findByCouponNameContaining(String keyword, Pageable pageable);
 }
